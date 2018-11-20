d9230e23d7356e8556b6aea6d19afce506a8e229
anisskiller <anissaallou36@gmail.com>
2018-11-20 09:50:03 +0100

    Update Pizza.java

--- pizzeria-console-objet/src/main/java/classe/Pizza.java
+++ pizzeria-console-objet/src/main/java/classe/Pizza.java
@@ -1,74 +1,98 @@
 package classe;
 
 public class Pizza {
-public String code;
-public String designation;
-public double prix;
+	
 
-/**
- * 
- * @param code => code de la pizza (Majuscule)
- * @param designation => nom de la pizza
- * @param prix => prix de la pizza
- */
+	
 
-// constructeur 
-public Pizza(String code, String designation, double prix) {
-	this.code = code;
-	this.designation = designation;
-	this.prix = prix;
-}
+	/** 
+	 * 
+	 */
+	private int id;
+	private String code;
+	private String désignation;
+	private double prix;
+	
+	private static  int compteur = 0;
+	
+	/**
+	 * 
+	 * @param code : designe le CODE de la pizza
+	 * @param désignation : Designe le nom utilitaire de la pizza
+	 * @param prix : Prix en euros de la pizza
+	 * @param id : Identifiant unique qui ajoute un au compteur static
+	 */
+	public Pizza(String code, String désignation, double prix) {
+		this.id = compteur++;
+		this.code = code;
+		this.désignation = désignation;
+		this.prix = prix;			
+	}
+	
+	public Pizza() {
+		// TODO Auto-generated constructor stub
+	}
 
+	/**
+	 * 
+	 * @return l'id de la pizza
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * 
+	 * @param id modifie l'id de la pizza
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * 
+	 * @return le code de la pizza
+	 */
+	public String getCode() {
+		return code;
+	}
+	/**
+	 * 
+	 * @param code de la pizza
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+	/**
+	 * 
+	 * @return la désignation de la pizza
+	 */
+	public String getDésignation() {
+		return désignation;
+	}
+	/**
+	 * 
+	 * @param désignation de la pizza
+	 */
+	public void setDésignation(String désignation) {
+		this.désignation = désignation;
+	}
+	/**
+	 * 
+	 * @return le prix en float de la pizza en euro
+	 */
+	public double getPrix() {
+		return prix;
+	}
+	/**
+	 * 
+	 * @param prix de la pizza en float
+	 */
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
 
-// getters 
-
-/**
- * 
- * @return retourne le code de la pizza
- * @return retourne le nom de la pizza
- * @return retourne le prix de la pizza
- */
-
-public String getCode() {
-	return code;
-}
-
-public String getDesignation() {
-	return designation;
-}
-
-public double getPrix() {
-	return prix;
-}
-
-
-// setters 
-
-/**
- * 
- * @param modifie le code de la pizza
- * @param modifie le nom de la pizza
- * @param modifie le prix de la pizza
- */
-public void setCode(String code) {
-	this.code = code;
-}
-
-public void setDesignation(String designation) {
-	this.designation = designation;
-}
-
-public void setPrix(double prix) {
-	this.prix = prix;
-}
-
-/**
- * 
- * @return affiche le menu d el'application
- */
-
-public static String afficheMenu() {
-	return "***** Pizzeria Administration *****\r\n" + "1.	  Lister les pizzas\r\n" + "2.	  Ajouter une nouvelle pizza\r\n" + "3.	  Mettre à jour une pizza\r\n" + "4.	  Supprimer une pizza\r\n" + "99.	  Sortir";
-}
+	@Override
+	public String toString() {
+		return code + " -> " + désignation + "("+ prix + "€)";
+	}
 	
 }
