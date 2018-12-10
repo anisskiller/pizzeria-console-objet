ea09dce34843acb1bf2f8d584f4c5aeb3b400a12
anisskiller <anissaallou36@gmail.com>
2018-12-10 09:46:01 +0100

    Update ConnectPizzeria.java

--- pizzeria-console-objet/src/main/java/com/pizzeria/ConnectPizzeria.java
+++ pizzeria-console-objet/src/main/java/com/pizzeria/ConnectPizzeria.java
@@ -9,8 +9,8 @@ import java.util.Properties;
 
 public class ConnectPizzeria {
 	public static Connection getConnection() throws Exception {
-//		
-//		DriverManager.setLogWriter(new PrintWriter(System.out));
+		
+		// DriverManager.setLogWriter(new PrintWriter(System.out));
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
 		String url = null;
@@ -19,7 +19,7 @@ public class ConnectPizzeria {
 		try{
 			  Properties properties = new Properties();
 		
-			  FileInputStream tmpFile = new FileInputStream("src/main/resources/connect.properties");
+			  FileInputStream tmpFile = new FileInputStream("src/main/java/classe/properties.properties");
 			  properties.load(tmpFile);
 			  url = properties.getProperty("url");
 			  user = properties.getProperty("user");
